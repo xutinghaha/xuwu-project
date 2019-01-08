ea008738dbb197534448d0bef41c57ef648092fa
dqcer <dqcer@sina.com>
2019-01-08 15:00:40 +0800

    添加邮件服务功能模块

--- src/test/java/com/xuting/xuwuproject/web/service/mail/MailServiceTest.java
+++ src/test/java/com/xuting/xuwuproject/web/service/mail/MailServiceTest.java
@@ -31,12 +31,12 @@ public class MailServiceTest extends XuwuProjectApplicationTests {
         context.setVariable("id", "000006");
         String emailContent = templateEngine.process("/email/emailTemplate", context);
 
-        mailService.sendHtmlMail("916466357@qq.com","主题：这是模板邮件",emailContent);
+        mailService.sendHtmlMail("228386474@qq.com","主题：这是模板邮件",emailContent);
     }
 
     @Test
     public void sendSimpleMail() {
-        mailService.sendSimpleMail("916466357@qq.com","test simple mail"," hello this is simple mail");
+        mailService.sendSimpleMail("228386474@qq.com","test simple mail"," hello this is simple mail");
     }
 
     @Test
@@ -46,13 +46,13 @@ public class MailServiceTest extends XuwuProjectApplicationTests {
                 "    <h3>hello world ! 这是一封html邮件!</h3>\n" +
                 "</body>\n" +
                 "</html>";
-        mailService.sendHtmlMail("916466357@qq.com","test simple mail",content);
+        mailService.sendHtmlMail("228386474@qq.com","test simple mail",content);
     }
 
     @Test
     public void sendAttachmentsMail() {
         String filePath="e:\\tmp\\application.log";
-        mailService.sendAttachmentsMail("916466357@qq.com", "主题：带附件的邮件", "有附件，请查收！", filePath);
+        mailService.sendAttachmentsMail("228386474@qq.com", "主题：带附件的邮件", "有附件，请查收！", filePath);
     }
 
     @Test
@@ -61,6 +61,6 @@ public class MailServiceTest extends XuwuProjectApplicationTests {
         String content="<html><body>这是有图片的邮件：<img src=\'cid:" + rscId + "\' ></body></html>";
         String imgPath = "C:\\Users\\summer\\Pictures\\favicon.png";
 
-        mailService.sendInlineResourceMail("916466357@qq.com", "主题：这是有图片的邮件", content, imgPath, rscId);
+        mailService.sendInlineResourceMail("228386474@qq.com", "主题：这是有图片的邮件", content, imgPath, rscId);
     }
 }
