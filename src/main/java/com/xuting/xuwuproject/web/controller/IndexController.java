e6455e9931b65f13a3803b57ac14b25665fc64ab
xutinghaha <228386474@qq.com>
2019-01-08 21:14:35 +0800

    全局错误配置

--- src/main/java/com/xuting/xuwuproject/web/controller/IndexController.java
+++ src/main/java/com/xuting/xuwuproject/web/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.xuting.xuwuproject.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,7 +16,34 @@ public class IndexController {
     @RequestMapping({"/","/index"})
     public String toIndex(Model model) {
 
-        Integer.parseInt("1.26");
+       /* Integer.parseInt("1.26");*/
         return "index";
     }
+
+    @GetMapping("404")
+    public String notFindPage() {
+        return "404";
+    }
+
+    /**
+     * 未授权
+     * @return
+     */
+    @GetMapping("403")
+    public String accessErro() {
+        return "403";
+    }
+
+    @GetMapping("500")
+    public String internelError() {
+        return "500";
+    }
+
+    @RequestMapping("/logout/page")
+    public String logoutPage() {
+        return "logout";
+    }
 }
+
+
+
